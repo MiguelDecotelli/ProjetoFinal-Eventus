654c3c5a7e659a5eaca427d3d3cbde9e7a9eee4e
Dênis Rocha <denischarocha@gmail.com>
2024-10-15 20:14:00 -0300

    Login input changed to floating style, no-after class created

--- eventus/src/components/Input.jsx
+++ eventus/src/components/Input.jsx
@@ -3,33 +3,35 @@
 import { forwardRef } from "react";
 
 const inputCSS =
-	"form-control border-0 bg-transparent custom-input px-0 pt-2 pb-0 shadow-none";
+	"form-control text-start border-0 bg-transparent custom-input px-0 pt-4 pb-0 shadow-none";
 
 export const Input = forwardRef(
 	({ label, type = "text", id, placeholder, error, ...rest }, ref) => {
 		return (
 			<>
 				<div className="input-group d-flex flex-nowrap border-bottom">
-					<label
-						className="input-group-text border-0 bg-transparent text p-0"
-						htmlFor={id}
-					>
-						{label}
-					</label>
-					<input
-						type={type}
-						className={inputCSS}
-						id={id}
-						placeholder={placeholder}
-						ref={ref}
-						{...rest}
-					/>
+					<div className="form-floating">
+						<input
+							type={type}
+							className={inputCSS}
+							id={id}
+							placeholder={placeholder}
+							ref={ref}
+							{...rest}
+						/>
+						<label
+							className="no-after bg-transparent"
+							style={{color: "var(--text)"}}
+							htmlFor={id}
+						>
+							{label}
+						</label>
+					</div>
 				</div>
 
 				<p
-					className={`alert text-danger p-0 m-0 error-message ${
-						error ? "visible" : "invisible"
-					}`}
+					className={`alert text-danger p-0 m-0 error-message ${error ? "visible" : "invisible"
+						}`}
 				>
 					{error}
 				</p>
