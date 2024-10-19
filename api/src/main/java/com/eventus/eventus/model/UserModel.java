469cc550a2a303f478dfc40d33534b3db221d344
Dênis Rocha <denischarocha@gmail.com>
2024-10-19 19:33:33 -0300

    Models created, userModel renamed

--- api/src/main/java/com/eventus/eventus/model/UserModel.java
+++ api/src/main/java/com/eventus/eventus/model/UserModel.java
@@ -6,22 +6,24 @@ import java.util.Date;
 
 @Entity
 @Data
-@Table(name="users")
+@Table(name="Users")
 public class UserModel {
     @Id
-    @Column
+    @Column(name="user_id")
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
-    @Column(nullable = false)
-    private String username;
-    @Column(nullable = false)
-    private String password;
-    @Column(nullable = false)
-    private String email;
-    @Column
-    private String name;
-    @Column
-    private String lastname;
-    @Column
-    private Date birthday;
+    private int userId;
+    @Column(name="user_username", nullable = false, unique = true)
+    private String userUsername;
+    @Column(name="user_password", nullable = false, unique = true)
+    private String userPassword;
+    @Column(name="user_email", nullable = false, unique = true)
+    private String userEmail;
+    @Column(name="user_name")
+    private String userName;
+    @Column(name="user_lastname")
+    private String userLastname;
+    @Column(name="user_birthday")
+    private Date userBirthday;
+    @Column(name="user_role")
+    private String userRole;
 }
