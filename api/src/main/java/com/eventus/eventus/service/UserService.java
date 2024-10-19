5bb7b1726a45b02b09ddd22715ca07309c50200e
Dênis Rocha <denischarocha@gmail.com>
2024-10-19 19:34:42 -0300

    User Service names refactored, more todos created for handling error

--- api/src/main/java/com/eventus/eventus/service/UserService.java
+++ api/src/main/java/com/eventus/eventus/service/UserService.java
@@ -27,41 +27,45 @@ public class UserService {
         return user.map(this::convertToDTO).orElse(null);
     }
     public UserDTO createUser(UserDTO userDTO){
+        // TODO: Handle Erros
         UserModel userModel = new UserModel();
-        userModel.setUsername(userDTO.getUsername());
-        userModel.setName(userDTO.getName());
-        userModel.setEmail(userDTO.getEmail());
-        userModel.setBirthday(userDTO.getBirthday());
-        userModel.setLastname(userDTO.getLastname());
+        userModel.setUserUsername(userDTO.getUserUsername());
+        userModel.setUserName(userDTO.getUserName());
+        userModel.setUserEmail(userDTO.getUserEmail());
+        userModel.setUserBirthday(userDTO.getUserBirthday());
+        userModel.setUserLastname(userDTO.getUserLastname());
         userRepository.save(userModel);
         return convertToDTO(userModel);
     }
     public UserDTO updateUser(int id, UserDTO userDTO){
+        // TODO: Handle if user has permissions
         Optional<UserModel> userOption = userRepository.findById(id);
         if(!userOption.isPresent()){
             return null;
         }
         UserModel userModel = new UserModel();
-        userModel.setUsername(userDTO.getUsername());
-        userModel.setName(userDTO.getName());
-        userModel.setEmail(userDTO.getEmail());
-        userModel.setBirthday(userDTO.getBirthday());
-        userModel.setLastname(userDTO.getLastname());
+        userModel.setUserUsername(userDTO.getUserUsername());
+        userModel.setUserName(userDTO.getUserName());
+        userModel.setUserEmail(userDTO.getUserEmail());
+        userModel.setUserBirthday(userDTO.getUserBirthday());
+        userModel.setUserLastname(userDTO.getUserLastname());
         userRepository.save(userModel);
         return convertToDTO(userModel);
     }
     public void deleteUser(int id){
         // TODO: Handle if user exists
+        // TODO: Handle if user has permissions
         // TODO: Handle errors
         userRepository.deleteById(id);
     }
     private UserDTO convertToDTO(UserModel userModel){
+        // TODO: Handle if user has permissions
         UserDTO userDTO = new UserDTO();
-        userDTO.setId(userModel.getId());
-        userDTO.setName(userModel.getName());
-        userDTO.setUsername(userModel.getUsername());
-        userDTO.setLastname(userModel.getLastname());
-        userDTO.setEmail(userModel.getEmail());
+        userDTO.setUserId(userModel.getUserId());
+        userDTO.setUserName(userModel.getUserName());
+        userDTO.setUserUsername(userModel.getUserUsername());
+        userDTO.setUserLastname(userModel.getUserLastname());
+        userDTO.setUserEmail(userModel.getUserEmail());
         return userDTO;
     }
 }
