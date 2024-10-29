583593dc670ee279fa928531b42fb818d90f9839
MiguelDecotelli <migueldecotelli@icloud.com>
2024-10-29 09:31:48 -0300

    Nova Home (incompleta).

--- eventus/src/pages/home/index.jsx
+++ eventus/src/pages/home/index.jsx
@@ -5,9 +5,9 @@ import { CardEvent } from "../../components/CardEvent";
 
 export const Home = () => {
 	return (
-		<div className="container">
+		<div className="container-carousel">
 			<Navbar />
-			<main className="py-2 d-flex flex-column gap-5">
+			<main className="d-flex flex-column gap-3">
 				<Carousel />
 				<h2>Titulo</h2>
 				<section className="d-flex gap-3 align-items-center justify-content-center">
