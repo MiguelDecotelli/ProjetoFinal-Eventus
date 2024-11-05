e33e644feb8dfc88f132658f2feb8f9e0a76ad26
Dênis Rocha <denischarocha@gmail.com>
2024-11-05 15:44:28 -0300

    Handle errors on User Service, AuthenticationService created, some minor bugs fixed

--- api/src/main/java/com/eventus/eventus/security/JwtAuthenticationFilter.java
+++ api/src/main/java/com/eventus/eventus/security/JwtAuthenticationFilter.java
@@ -1,12 +1,10 @@
 package com.eventus.eventus.security;
 
 import com.eventus.eventus.model.UserModel;
-import com.eventus.eventus.service.UserService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
