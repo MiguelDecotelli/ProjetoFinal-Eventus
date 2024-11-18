a0eecf2a16899a6db3394e092c29dae594f4de33
JefersonLanius <jefersondanielanius@gmail.com>
2024-11-18 20:03:38 -0300

    version 2 of db

--- db/eventus_002.sql
+++ db/eventus_002.sql
@@ -1,5 +1,3 @@
-CREATE SCHEMA IF NOT EXISTS `eventus` ;
-USE `eventus` ;
 
 -- -----------------------------------------------------
 -- Table `mydb`.`Cities`
@@ -34,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `eventus`.`Users` (
   INDEX `fk_Users_Cities1_idx` (`Cities_city_id` ASC) VISIBLE,
   CONSTRAINT `fk_Users_Cities1`
     FOREIGN KEY (`Cities_city_id`)
-    REFERENCES `mydb`.`Cities` (`city_id`)
+    REFERENCES `eventus`.`Cities` (`city_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -43,21 +41,21 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table `mydb`.`EventAddress`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `eventus`.`EventAddress` (
-  `EventAddress_id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `eventus`.`Address` (
+  `Address_id` INT NOT NULL AUTO_INCREMENT,
   `cep` VARCHAR(45) NULL,
   `street` VARCHAR(45) NULL,
   `street_number` INT NOT NULL,
   `complement` VARCHAR(45) NULL,
   `description` VARCHAR(45) NULL,
-  `Cities_city_id` INT NOT NULL,
-  PRIMARY KEY (`EventAddress_id`, `Cities_city_id`),
-  UNIQUE INDEX `EventAddress_id_UNIQUE` (`EventAddress_id` ASC) VISIBLE,
+  `City_id` INT NOT NULL,
+  PRIMARY KEY (`Address_id`, `City_id`),
+  UNIQUE INDEX `Address_id_UNIQUE` (`Address_id` ASC) VISIBLE,
   UNIQUE INDEX `number_UNIQUE` (`street_number` ASC) VISIBLE,
-  INDEX `fk_EventAddress_Cities1_idx` (`Cities_city_id` ASC) VISIBLE,
-  CONSTRAINT `fk_EventAddress_Cities1`
-    FOREIGN KEY (`Cities_city_id`)
-    REFERENCES `mydb`.`Cities` (`city_id`)
+  INDEX `fk_Address_Cities1_idx` (`Cities_city_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Address_Cities1`
+    FOREIGN KEY (`City_id`)
+    REFERENCES `eventus`.`Cities` (`city_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -67,20 +65,20 @@ ENGINE = InnoDB;
 -- Table `mydb`.`Events`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `eventus`.`Events` (
-  `event_id` INT NOT NULL AUTO_INCREMENT,
-  `event_name` VARCHAR(45) NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
   `initial_date` VARCHAR(45) NOT NULL,
   `final_date` VARCHAR(45) NOT NULL,
   `description` TEXT(2550) NOT NULL ,
-  `event_image` BLOB NOT NULL,
-  `event_status` ENUM("Active", "Concluded", "Pending", "Postponed") NOT NULL,
-  `EventAddress_EventAddress_id` INT NOT NULL,
-  PRIMARY KEY (`event_id`, `EventAddress_EventAddress_id`),
+  `image` BLOB NOT NULL,
+  `status` ENUM("Active", "Concluded", "Pending", "Postponed") NOT NULL,
+  `Address_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `Address_id`),
   UNIQUE INDEX `id_events_UNIQUE` (`event_id` ASC) VISIBLE,
-  INDEX `fk_Events_EventAddress1_idx` (`EventAddress_EventAddress_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Events_EventAddress1`
-    FOREIGN KEY (`EventAddress_EventAddress_id`)
-    REFERENCES `mydb`.`EventAddress` (`EventAddress_id`)
+  INDEX `fk_Events_Address1_idx` (`Address_Address_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Events_Address1`
+    FOREIGN KEY (`Address_id`)
+    REFERENCES `eventus`.`Address` (`Address_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -90,13 +88,13 @@ ENGINE = InnoDB;
 -- Table `mydb`.`Tickets`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `eventus`.`Tickets` (
-  `ticket_id` INT NOT NULL AUTO_INCREMENT,
-  `ticket_name` VARCHAR(255) NULL,
-  `ticket_value` FLOAT NULL,
-  `ticket_description` TEXT(2550) NULL,
-  `ticket_amount` INT NULL,
-  PRIMARY KEY (`ticket_id`),
-  UNIQUE INDEX `ticket_id_UNIQUE` (`ticket_id` ASC) VISIBLE)
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NULL,
+  `value` FLOAT NULL,
+  `description` TEXT(2550) NULL,
+  `amount` INT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
@@ -105,22 +103,21 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `eventus`.`UsersTickets` (
   `UsersTickets_id` INT NOT NULL AUTO_INCREMENT,
-  `Users_user_id` INT NOT NULL,
-  `Tickets_ticket_id` INT NOT NULL,
+  `Users_id` INT NOT NULL,
+  `Tickets_id` INT NOT NULL,
   `ticket_status` ENUM("Active", "Cancelled", "Pending", "Expired") NOT NULL,
   `api_response` BLOB NOT NULL,
   INDEX `fk_Users_has_Tickets_Tickets1_idx` (`Tickets_ticket_id` ASC) VISIBLE,
   INDEX `fk_Users_has_Tickets_Users1_idx` (`Users_user_id` ASC) VISIBLE,
   PRIMARY KEY (`UsersTickets_id`),
   CONSTRAINT `fk_Users_has_Tickets_Users1`
-    FOREIGN KEY (`Users_user_id`)
-    REFERENCES `mydb`.`Users` (`user_id`)
+    FOREIGN KEY (`Users_id`)
+    REFERENCES `eventus`.`Users` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Users_has_Tickets_Tickets1`
-    FOREIGN KEY (`Tickets_ticket_id`)
-    REFERENCES `mydb`.`Tickets` (`ticket_id`)
+    FOREIGN KEY (`Tickets_id`)
+    REFERENCES `eventus`.`Tickets` (`ticket_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
-
