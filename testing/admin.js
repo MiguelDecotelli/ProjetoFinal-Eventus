9699c1df6ff61ce11aa36609c76508b5eccb2ef1
Dênis Rocha <denischarocha@gmail.com>
2024-11-19 13:43:35 -0300

    cities scripts added

---
+++ testing/admin.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+async function login(){
+	const admin = {"username": "denis", "password":"1234"}
+	const response = await axios.post("http://localhost:8080/api/auth/login", admin)
+		.catch(_=>{console.log("Erro ao logar com o Admin")}
+	);
+	return response.data;
+}
+export default login;
