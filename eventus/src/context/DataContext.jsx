485043b219084c83358664cd9099d33b232730b1
Dênis Rocha <denischarocha@gmail.com>
2024-11-21 11:04:55 -0300

    Get events done

--- eventus/src/context/DataContext.jsx
+++ eventus/src/context/DataContext.jsx
@@ -1,30 +1,32 @@
-import{ createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect } from "react";
+import { makeRequest } from "../utils/makeRequest.js";
 
 export const DataContext = createContext();
 
 export const DataProvider = ({ children }) => {
-  const [eventos, setEventos] = useState([]);
+	const [eventos, setEventos] = useState([]);
 
-  const url = "https://jsonplaceholder.typicode.com/posts/";
+	const url = "http://localhost:8080/api/events";
 
-  // Função para buscar os eventos
-  const fetchEventos = async () => {
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      setEventos(data);
-    } catch (error) {
-      console.error("Erro ao buscar eventos:", error);
-    }
-  };
+	// Função para buscar os eventos
+	const fetchEventos = async () => {
+		try {
+			const response = await fetch(url);
+			const data = await response.json();
+			console.log(data);
+			setEventos(data);
+		} catch (error) {
+			console.error("Erro ao buscar eventos:", error);
+		}
+	};
 
-  useEffect(() => {
-    fetchEventos();
-  }, []);
+	useEffect(() => {
+		fetchEventos();
+	}, []);
 
-  return(
-    <DataContext.Provider value={{ eventos }}>
-      {children}
-    </DataContext.Provider>
-  )
+	return (
+		<DataContext.Provider value={{ eventos }}>
+			{children}
+		</DataContext.Provider>
+	)
 };
